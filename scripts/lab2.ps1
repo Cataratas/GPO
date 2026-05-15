3ba59dd1d2e2264f348f8a3989c23a08ddae9455
nti-exponencial <nti@colegioexponencial.com.br>
2026-05-15 14:20:27 -0300

    global

--- scripts/lab2.ps1
+++ scripts/lab2.ps1
@@ -46,8 +46,8 @@ New-Item -Path "HKU:\TempHive\Software\Microsoft\Windows\CurrentVersion\Policies
 New-ItemProperty -Path "HKU:\TempHive\Software\Microsoft\Windows\CurrentVersion\Policies\System" `
     -Name "Wallpaper" -PropertyType String -Value "C:\Windows\Web\Wallpaper\wallpaper_blue.jpeg" -Force
 
-New-Item -Path "HKU:\TempHive\SOFTWARE\Policies\Microsoft\Windows\Personalization" -Force | Out-Null
-New-ItemProperty -Path "HKU:\TempHive\SOFTWARE\Policies\Microsoft\Windows\Personalization" -Name "NoChangingLockScreen" -PropertyType DWord -Value 1 -Force
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization" -Force | Out-Null
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization" -Name "NoChangingLockScreen" -PropertyType DWord -Value 1 -Force
 
 # (2 = stretch, 0 = center, 6 = fit, 10 = fill)
 New-ItemProperty -Path "HKU:\TempHive\Software\Microsoft\Windows\CurrentVersion\Policies\System" `
